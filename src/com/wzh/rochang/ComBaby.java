5ea8a50bc871840aef2c41b6831a98c1e4b0f865
wangzh13 <wangzh13@asiainfo.com>
2020-03-31 10:08:48 +0800

    推送测试

---
+++ src/com/wzh/rochang/ComBaby.java
@@ -0,0 +1,13 @@
+package com.wzh.rochang;
+
+public class ComBaby {
+    private String didi;
+
+    public String getDidi() {
+        return didi;
+    }
+
+    public void setDidi(String didi) {
+        this.didi = didi;
+    }
+}
